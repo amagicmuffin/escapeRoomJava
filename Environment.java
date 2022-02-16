3c8464a909277a9551add3e22e5c5f9bcfd87464
amagicmuffin <62270558+amagicmuffin@users.noreply.github.com>
2022-02-16 10:27:23 -0800

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 public class Environment {
 
     ///////// room things /////////
-    public static String room = null; // null represents not in a room. for example, in start screen menu
+    public static String currentRoom = null; // null represents not in a room. for example, in start screen menu
 
     public static Room[][] map = configs.map;
 
     ///////// entity things /////////
     public static ArrayList<Entity> tiles = new ArrayList<Entity>();
+    tiles.add(new Entity("debug chest 1", 'X', 2, 2, 0, 0));
 
     /** test program **/
     public static void main(String[] args) {
